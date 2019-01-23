e9799998113d82192f362ac2e5be61b9c43c1483
Vitor George <vitor.george@gmail.com>
2019-01-23 11:12:43 +0000

    Remove "Develop for" banner

--- _config.js
+++ _config.js
@@ -1,15 +1,7 @@
 module.exports = {
   baseurl: 'http://developmentseed.github.io/gep-docs',
   title: 'Global Electrification Platform',
-  description: 'Documentation',
-
-  attribution: {
-    role: 'Developed for',
-    name: 'Generic Company',
-    logo: 'generic-logo-pos-hor.svg',
-    url: 'https://developmentseed.org'
-  },
-
+  description: 'Data ingest documentation',
   sectionsMeta: {
     's0-introduction': 'Introduction',
     's1-preparing-the-data': 'Preparing the data',
